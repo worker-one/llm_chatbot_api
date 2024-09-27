6174f3239f82681e6a15a0366836c2f8eb43bc3e
worker-one <konst.verner.org@gmail.com>
2024-09-27 14:53:54 +0200

    Update users.py

--- src/llm_chatbot_api/api/routes/users.py
+++ src/llm_chatbot_api/api/routes/users.py
@@ -1,12 +1,12 @@
 import logging
 
-from fastapi import APIRouter
-from llm_chatbot_api.api import schemas
-from llm_chatbot_api.db import models
-from llm_chatbot_api.db.crud import read_user, upsert_user, read_users, delete_user
+from fastapi import APIRouter, HTTPException
 from omegaconf import OmegaConf
 from sqlalchemy.orm import Session
-from fastapi import HTTPException
+
+from llm_chatbot_api.api import schemas
+from llm_chatbot_api.db import models
+from llm_chatbot_api.db.crud import read_users, upsert_user
 
 
 # Load logging configuration with OmegaConf
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
-@router.post("/add_user")
+@router.post("/users")
 def add_user(request: schemas.AddUserRequest):
     user = request.user
 
@@ -24,27 +24,16 @@ def add_user(request: schemas.AddUserRequest):
     upsert_user(user.id, user.name)
     return {"message": f"User `{user.name}` added successfully."}
 
-@router.get("/user_exists")
-def get_users(user_id: int) -> dict[str, bool]:
-    db_user = read_user(user_id)
-    if not db_user:
-        return {"response": False}
-    else:
-        return {"response": True}
-
-@router.get("/get_users")
+@router.get("/users")
 def get_users() -> list[schemas.User]:
     db_users = read_users()
     users = [schemas.User(id=db_user.id, name=db_user.name) for db_user in db_users]
     return users
 
-@router.post("/delete_user")
-def remove_user(user_id: int):
-    logger.info(f"Deleting user with id: {user_id}")
-    try:
-        # Call the delete_user function from the CRUD module
-        delete_user(user_id)
-        return {"message": f"User with id `{user_id}` deleted successfully."}
-    except Exception as e:
-        logger.error(f"Error deleting user with id {user_id}: {e}")
-        raise HTTPException(status_code=500, detail=f"Error deleting user with id `{user_id}`.")
+@router.get("/users/{user_id}")
+def get_user(user_id: int) -> schemas.User:
+    db_users = read_users()
+    users = [schemas.User(id=db_user.id, name=db_user.name) for db_user in db_users if user_id==db_user.id]
+    if not users:
+        raise HTTPException(status_code=404, detail=f"User `user_id` not found")
+    return schemas.User(id=users[0].id, name=users[0].name)
